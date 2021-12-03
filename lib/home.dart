d6402f339b774da74368d1c00498a0da57ccb60e
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2021-12-03 23:46:09 +0100

    initialising repository

---
+++ lib/home.dart
@@ -0,0 +1,88 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  Widget customCard(){
+    return Padding(padding: const EdgeInsets.all(20.0),
+    child: InkWell(
+      child: Material(
+        color: Colors.indigoAccent,
+        elevation:20.0,
+        child: Container(
+          child: Column(
+            children:const[
+              Padding(padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Material(
+                elevation: 5.0,
+                
+                child: ClipOval(
+                  child: Image(
+                    width: 200.0,
+                    height: 200.0,
+                    image:  AssetImage(
+                      
+                    "assets/yisafa.jpg",
+                    
+                  ),)
+                ),
+              ),
+              ),
+
+              Center(
+                child: Text('Python',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                  color: Colors.white
+                ),
+                
+                ),
+              ),
+
+          
+            // ignore: unnecessary_const
+           const Center(
+              // ignore: unnecessary_const
+              child: const Container(
+                  padding: EdgeInsets.all(20.0),
+                  child:  Text('This is a Quiz Application',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+            ),
+
+            ],
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quizzer'),
+        leading: const Icon(Icons.menu),
+      ),
+      body: ListView(
+        children: [
+          customCard(),
+        ],
+    
+      ),
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,40 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:quizzy/home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const Homepage(),));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Text(
+          'welcome',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
