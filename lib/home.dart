f223e522d1a5c67d4984ca931c35c2abbb76710b
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2021-12-15 23:38:26 +0100

    adding more cards and fixing card styling

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,67 +11,61 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
-  Widget customCard(){
-    return Padding(padding: const EdgeInsets.all(20.0),
-    child: InkWell(
-      child: Material(
-        color: Colors.indigoAccent,
-        elevation:20.0,
-        child: Container(
-          child: Column(
-            children:const[
-              Padding(padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Material(
-                elevation: 5.0,
-                
-                child: ClipOval(
-                  child: Image(
-                    width: 200.0,
-                    height: 200.0,
-                    image:  AssetImage(
-                      
-                    "assets/yisafa.jpg",
-                    
-                  ),)
-                ),
-              ),
-              ),
-
-              Center(
-                child: Text('Python',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 24,
-                  color: Colors.white
-                ),
-                
-                ),
-              ),
-
-          
-            
-           Center(
-             
-            
-              // ignore: unnecessary_const
-              child:  Text('This is a Quiz Application',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+  Widget customCard() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: InkWell(
+        child: Material(
+          color: Colors.indigoAccent,
+          elevation: 20.0,
+          child: Container(
+            child: Column(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                    child: ClipOval(
+                        child: Image(
+                      width: 200.0,
+                      height: 200.0,
+                      image: AssetImage(
+                        "assets/yisafa.jpg",
+                      ),
+                    )),
                   ),
                 ),
-          
-            
-
-            ],
+                Center(
+                  child: Text(
+                    'Python',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                        color: Colors.white),
+                  ),
+                ),
+                Center(
+                  // ignore: unnecessary_const
+                  child: Text(
+                    'This is a Quiz Application',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,9 +76,15 @@ class _HomepageState extends State<Homepage> {
       body: ListView(
         children: [
           customCard(),
+          customCard(),
+          customCard(),
+          customCard(),
+          customCard(),
+          customCard()
         ],
-    
       ),
     );
   }
 }
+
+class Circular {}
